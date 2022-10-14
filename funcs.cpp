13c97aa16d97b66b123e18b17d8660aa8d45372d
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-14 19:07:26 -0400

    wrote the basics

---
+++ funcs.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <cctype>
+
+std::string removeLeadingSpaces(std::string line){
+	
+}
+
---
+++ funcs.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+#pragma once 
+
+std::string removeLeadingSpaces(std::string line);
+
