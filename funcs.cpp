a4d68912eda8f4f95263e586c67f0cd8192273cc
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-19 19:18:41 -0400

    updated

--- funcs.cpp
+++ funcs.cpp
@@ -6,7 +6,7 @@ std::string removeLeadingSpaces(std::string line){
 	std::string new_string;
 	int i = 0;
 	char c;
-
+	
 	while (i < line.size()){
 		c = line[i];
 		if(!isspace(c)) {
@@ -17,5 +17,37 @@ std::string removeLeadingSpaces(std::string line){
 	new_string = line.substr(i);
 
 	return new_string;
+}
+
+int countChar(std::string line, char c) {
+	int count = 0;
+	
+	for(int i = 0; i < line.length(); i++){
+		if(line[i] == c ){
+			count++;
+		}
+	}
+
+	return count;
+}
+
+std::string indent(std::string line){
+	int block, open, close = 0;
+	std::string indented;
+	
+	int i = 0; 
+	while(i < line.length()) {
+		open += countChar(line, '{');
+		close += countChar(line, '}');
+		block = open - close;
+		for(int j = 0; i < block; j++)
+		{
+			indented += '\t';	
+		}
+		indented += line;	
+		i++;		
+	}
+	
+	return indented;
 }
 
--- funcs.h
+++ funcs.h
@@ -3,4 +3,4 @@
 
 std::string removeLeadingSpaces(std::string line);
 int countChar(std::string line, char c);
-
+std::string indent(std::string line);
