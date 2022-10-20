edb07480862f8f87be321a2d19cbf9d68f902bc6
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-19 22:31:54 -0400

    file

--- bad-code.cpp
+++ bad-code.cpp
@@ -1,7 +1,8 @@
                 int main(){
-				   int x = 1; 
-     for(int i = 0; i < 10; i++) {
-				        cout << i;
-				                  cout << endl;
-				                   }
-				                       }
+           // Hi, I'm a program!
+int x = 1; 
+    for(int i = 0; i < 10; i++) {
+cout << i;
+          cout << endl;
+ }
+    }
