29b32bc0975860061b9d7b1649b43d9b543682f8
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-19 20:30:20 -0400

    adding file

---
+++ bad-code.cpp
@@ -0,0 +1,7 @@
+                int main(){
+				   int x = 1; 
+     for(int i = 0; i < 10; i++) {
+				        cout << i;
+				                  cout << endl;
+				                   }
+				                       }
