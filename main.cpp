fd2baecc428792470798f17f3d7808be630e5985
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-10-20 11:56:14 -0400

    removed unneeded code

--- main.cpp
+++ main.cpp
@@ -8,43 +8,32 @@ int main() {
 	std::cout << removeLeadingSpaces("           int c = -9;") << std::endl;
 	std::cout << countChar("for(int i = 0; i < 10; i++){", '{') << std::endl;
 
-
 	std::ifstream in_file;
-	in_file.open("bad-code.cpp");
-	if (in_file.fail()) { 
-		std::cout << "Failed" << std::endl;
-		return -1; 
-	}
-
 	std::ofstream out_file;
-	out_file.open("unindented-code.cpp");
-	if (out_file.fail()) { 
+	in_file.open("bad-code.cpp");
+	if (in_file.fail()) {
 		std::cout << "Failed" << std::endl;
-		return -1; 
-	}	
-
-	std::string line;
-	while(getline(in_file, line)){
-		out_file << removeLeadingSpaces(line) << std::endl;
+		return -1;
 	}
-
-	in_file.close();
-	out_file.close();
-
-	in_file.open("unindented-code.cpp");
 	out_file.open("good-code.cpp");
-
+	if (out_file.fail()) {
+		std::cout << "Failed" << std::endl;
+		return -1;
+	}
+	
 	int open = 0;
 	int close = 0;
 	int block = 0;
-	
+
+	std::string line;	
 	while(getline(in_file, line)) {
+		std::string str = removeLeadingSpaces(line);
 		block = open - close;
 		open += countChar(line, '{');
 		close += countChar(line, '}');
 
 		std::string tabs = "";
-		if(line[0] == '}') {
+		if(str[0] == '}') {
 			for (int i = 0; i < block - 1; i++) {
 				tabs += '\t';
 			}
@@ -55,7 +44,7 @@ int main() {
 			}
 			
 		}
-		out_file << tabs << line << std::endl;
+		out_file << tabs << str << std::endl;
 		
 		std::cout << open << " " << close << " " << block << " " << " END" <<std::endl;
 	}
